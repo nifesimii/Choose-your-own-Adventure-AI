5681fb40fce23d731c587b8114bec4cc305dbbaf
nifesimii <nifesimifrank@gmail.com>
2025-11-30 21:17:51 +0100

    changed origin main

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,3 +1,3 @@
 //export const API_BASE_URL = "/choreo-apis/choose-your-own-adventure/backend/v1/api"
 
-export const API_BASE_URL = "/api"
+export const API_BASE_URL = "/choreo-apis/choose-your-own-adventure/backend/v1/api"
